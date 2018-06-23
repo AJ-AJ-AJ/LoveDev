00bfc87c024e198f58908f66ad9b1f8f6e3f938d
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-06-23 08:24:04 -0400

    completed express app.js file set up

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
 var logger = require('morgan');
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI); 
+
+//is mongo connected?
+
+const connection = mongoose.connection;
+connection.on('connected', () => {
+    console.log('Mongoose Connected Successfully');
+})
+
+//if connection has a error
+connection.on('error', () => {
+    console.log('Mongoose default connection error: ' + err)
+})
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI); 
 
 app.use(logger('dev'));
 app.use(express.json());
