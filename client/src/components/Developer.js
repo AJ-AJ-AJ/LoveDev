35bb179c8a97bd464a76d46e0e3f2c85a3703729
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-06-26 14:09:46 -0400

    created developer component

---
+++ client/src/components/Developer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Developer extends Component {
+    render() {
+        return (
+            <div>
+                Hello From developers
+            </div>
+        )
+    }
+}
+
+export default Developer
