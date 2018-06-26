2ccee3ae05187866851e15dc7d65c682e7f37b45
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-06-26 14:11:23 -0400

    added user model to route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const {UsersModel} = require('../db/schema')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+//send me the users
+
+// router.get(':/id', (req,res) => {
+//   UsersModel.findById(req.params.id).then((user) => {
+//     res.send({user})
+//   }
+// )})
 
 module.exports = router;
