b329cba9e05c973224cad05ce48850f21b5c4ce8
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-06-26 18:59:20 -0400

    added routing to react App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,51 @@
 import React, { Component } from 'react';
-import {Link, Switch, BrowserRouter as Router, Route} from 'react-router-dom'
+import { Link, Switch, BrowserRouter as Router, Route } from 'react-router-dom'
 import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
-import Users from './components/Users'
+import Users from './components/UsersLanding'
 import Developer from './components/DeveloperAdd'
 import HomePage from './components/HomePage'
 
 class App extends Component {
-  state={
-    users:[]
+  state = {
+    users: []
   }
 
   //get all of my users
-  componentDidMount(){
+  componentDidMount() {
     axios.get('/api/users').then(((res) => {
-      this.setState({users: res.data.users})
+      this.setState({ users: res.data.users })
     })).catch((err) => {
       console.log(err)
     })
   }
 
+  //wrap your UsersLanding in a wrapper to pass in the users props in a clean way
   render() {
+
+    const UsersWrapper = (props) => (
+      <Users users={this.state.users}{...props} />
+    )
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React AJ Jimmy</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Users/>
-      </div>
+      <Router>
+        <div>
+          <div>
+            <Link to='/'>
+              Home
+              </Link>
+            <Link to='usersLogin'>
+              Get Started
+              </Link>
+          </div>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/usersLogin' component={UsersWrapper} />
+            <Route exact path='/users/:userId' component={Developer} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
