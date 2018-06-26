371553945b79134c6a3e8addf4d7449295439b3c
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-06-26 14:11:08 -0400

    added developer model to route

--- routes/developer.js
+++ routes/developer.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const {DeveloperModel} = require('../db/schema')
 
 /* GET users listing. */
-router.get('/developer', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
