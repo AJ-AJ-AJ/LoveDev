c0aafabebce9a75ed78d2d53daabf8726d9b1db4
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-07-02 13:21:37 -0400

    positioned lovedev and boxes on hompepage screen

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
 class HomePage extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <div className="homeText">
-                <h1>LoveDev</h1>
-                <h3></h3>
+                <h1 className="love">LoveDev</h1>
+                <h3>Create <span>Share</span> Inspire </h3>
+
+                <div className="boxHolder">
+                    <div className="bigBox"></div>
+                    <div className="bigBox"></div>
+                    <div className="bigBox"></div>
+                </div>
+
             </div>
         )
     }
