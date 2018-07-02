ec8551439b88c404cb835d539e9dadaa7e2bb142
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-07-02 13:02:49 -0400

    removing link tag decorations from all links in react

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 class HomePage extends Component {
     render(){
         return(
-            <div>
-                Hello from Homepage
+            <div className="homeText">
+                <h1>LoveDev</h1>
+                <h3></h3>
             </div>
         )
     }
